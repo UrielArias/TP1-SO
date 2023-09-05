7d22cdb07166dbc84364edc6cf43ed804cdb3a8f
solrodriguez02 <solrodriguez@itba.edu.ar>
2023-09-05 15:16:33 +0000

    Check arguments

--- aplication.c
+++ aplication.c
@@ -1,3 +1,23 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+
+int main (int argc, char * argv []) {
+    if (argc != 2) {
+        if (argc > 2) 
+            fprintf(stderr, "Too many arguments\n");
+        else
+            fprintf(stderr, "There is one missing argument\n");
+        exit(EXIT_FAILURE);
+    }
+
+    DIR * dir = opendir(argv[1]);
+
+    if(dir == NULL) {
+        fprintf(stderr, "Directory %s is not valid\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+}
+

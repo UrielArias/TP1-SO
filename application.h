a1efd5a016835a301082aa1380a9fd75e4d59f10
UrielArias <urielarias.a12@gmail.com>
2023-09-07 02:40:45 -0300

    added createSlaves function & modularization

---
+++ application.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/select.h>
+
+#define MAXSLAVE 5
+#define FDSPERPIPE 2
+#define PIPESPERPROC 2
+#define NUMDEFAULTFDS 3
+#define MAXFILESPERSLAVE 2
+#define STDIN 0
+#define STDOUT 1
+#define READFILEPIPE 0
+#define WRITEFILEPIPE 1
+#define READRESULTPIPE 2
+#define WRITERESULTPIPE 3
+
+
+typedef struct process{
+    int pid;
+    int readFrom;
+    int sendTo;
+} process;

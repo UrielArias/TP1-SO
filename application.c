68e0e9812f15f18b6c31503f83c490c6ac5ff9ec
UrielArias <urielarias.a12@gmail.com>
2023-09-12 17:25:42 -0300

    Adding communication through pipes & write to file

--- application.c
+++ application.c
@@ -2,34 +2,42 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include "application.h"
 
+static int setFdsToCheck(process* slaves, int numSlaves, fd_set * set);
+static int sendFilesToSlave(process* slave, int numFiles, char** files);
+static int setFdsToCheck(process* slaves, int numSlaves, fd_set * set);
+
+
 int main (int argc, char * argv []) {
     if (argc < 2) {
         fprintf(stderr, "No arguments passed. A path to the files is needed\n");
         exit(EXIT_FAILURE);
     }
     int remainingFiles = argc - 1;    
-    int numSlaves = (argc-1 < 5) ? argc-1 : MAXSLAVE;
-    int filesPerSlave = ((argc - 1) < numSlaves)? 1 : MAXFILESPERSLAVE;
-
+    int numSlaves = (remainingFiles < 5) ? remainingFiles : MAXSLAVE;
+    int filesPerSlave = (remainingFiles <= numSlaves || remainingFiles <= MAXFILESPERSLAVE * MAXSLAVE)? 1 : MAXFILESPERSLAVE;
     FILE * result;
     if( (result = fopen("result.txt", "w")) == NULL ) {
         fprintf(stderr, "Fopen failed");
     }
+    //code;
     process slaves[numSlaves];
 
 
-    createSlaves(argc, slaves, argv++, filesPerSlave);
-    
+    createSlaves(numSlaves, slaves);    
+    sendInitialLoad(slaves, remainingFiles, argv, filesPerSlave);
+    monitorSlaves(slaves, remainingFiles, numSlaves, argv, result);
+    fclose(result);
 }
 
-void createSlaves(int numSlaves, process* slaveList, char ** files, int workload){
-    char * argvFork[MAXFILESPERSLAVE+2];
+void createSlaves(int numSlaves, process* slaveList){
+    char * argvFork[MAXFILESPERSLAVE];
     argvFork[0] = "slave";
+    argvFork[1] = NULL;
     char * envpFork[] = {NULL};
 
     int pipeFds[FDSPERPIPE * PIPESPERPROC];
     int returnValue, pid;
-    int maxFd = NUMDEFAULTFDS + workload * PIPESPERPROC * FDSPERPIPE;
+    int maxFd = NUMDEFAULTFDS + numSlaves * PIPESPERPROC * FDSPERPIPE;
     int fdsToClose[maxFd - NUMDEFAULTFDS];
     
     for (int slave = 0, index = 0; slave < numSlaves; slave++){
@@ -42,7 +50,7 @@ void createSlaves(int numSlaves, process* slaveList, char ** files, int workload
         returnValue = pipe(&pipeFds[READRESULTPIPE]);
 
         fdsToClose[index++] = pipeFds[READRESULTPIPE]; 
-        
+
         pid = fork();
 
         if (pid == 0){
@@ -53,27 +61,35 @@ void createSlaves(int numSlaves, process* slaveList, char ** files, int workload
             }
             dup(pipeFds[READFILEPIPE]);
             dup(pipeFds[WRITERESULTPIPE]);
-            for (int arg = 1; arg <= workload; arg++){
-                argvFork[arg] = *files;
-                files++;
-            }
-            argvFork[workload + 1] = NULL;
+            close(pipeFds[READFILEPIPE]);
+            close(pipeFds[WRITERESULTPIPE]);
             execve("./slave", argvFork, envpFork);
         }
         if (pid != 0){
             slaveList[slave].pid = pid;
             slaveList[slave].readFrom = pipeFds[READRESULTPIPE];
             slaveList[slave].sendTo = pipeFds[WRITEFILEPIPE];
+            slaveList[slave].isOperative= TRUE;
             close(pipeFds[READFILEPIPE]);
             close(pipeFds[WRITERESULTPIPE]);
         }
     }
 }
 
-void monitorSlaves(process* slaves, int remainingFiles, int numSlaves){
+int sendInitialLoad(process* slaves, int numSlaves, char ** files, int workload){
+    int ret;
+    for (int slave = 0; slave < numSlaves; slave++){
+        ret = sendFilesToSlave(&slaves[slave], workload, files);
+    } 
+    return ret;
+}
+
+void monitorSlaves(process* slaves, int remainingFiles, int numSlaves, char * argv [], FILE * resultFile){
     fd_set set;
     int filesDone = 0;
     int maxfd, activeFd;
+    int filesToAssigned = remainingFiles;
+    char buf[160];
     while (filesDone <= remainingFiles){
        //select needs the first argument to be the number of the greatest fds to check-> we want to check only the ones who are still are working
        //int select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval *timeout);
@@ -87,19 +103,43 @@ void monitorSlaves(process* slaves, int remainingFiles, int numSlaves){
         for (int slave = 0; activeFd> 0 && slave < numSlaves; slave++){
             //if
             if (FD_ISSET(slaves[slave].readFrom, &set)){
-                //tera
+                read(slaves[slave].readFrom, buf, 160);
+                slaves[slave].remainingTasks--;
+                filesDone++;
+                if (!slaves[slave].remainingTasks && filesToAssigned > 0){
+                    sendFilesToSlave(&slaves[slave], 1, argv);
+                    filesToAssigned--;
+                }
+                else if (!slaves[slave].remainingTasks){
+                    slaves[slave].isOperative = FALSE;
+                    //close fd? pipe no longer needed
+                }
+                //write to Shm se tiene que crear desde la app para tener el tad
+                fwrite(buf, sizeof(buf), 1, resultFile);
             }
         }
     }
-
 }
 
-int setFdsToCheck(process* slaves, int numSlaves, fd_set * set){
+static int sendFilesToSlave(process* slave, int numFiles, char** files){
+    if (numFiles <= 0 || files == NULL){
+        perror("An array of strings should be passed and it should be specified the amount to be passed");
+        exit(1);
+    }
+    int result;
+    for (int indexFile = 0; indexFile < numFiles; indexFile++, files++){
+        char * file = strcat(*files, " ");
+        result = write(slave->sendTo, file, strlen(*files));
+    }
+    return result;
+}
+
+static int setFdsToCheck(process* slaves, int numSlaves, fd_set * set){
     FD_ZERO(set); //first step to clear set
     int fd;
     int maxFd = -1; //in case there are no slaves left working it returns -1
     for (int slave = 0; slave < numSlaves; slave++){
-        if (slaves[slave].isExecuting){
+        if (slaves[slave].isOperative){
             fd = slaves[slave].readFrom;
             FD_SET(fd, set);
             maxFd = (fd > maxFd)? fd : maxFd;
